5ac828376b5b5d873b28fcfa82dcb04d858bc312
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-04-06 16:59:23 -0500

    video has been uploaded and has been set to autoplay.

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
+import { heroVideo, smallHeroVideo } from '../Utils/Index'
+import { useState } from 'react'
 
 function Hero() {
+
+    const [videoSrc, setVideoSrc] = useState(Window.innerWidth < 760 ? smallHeroVideo : heroVideo)
 
     useGSAP(() => {
         gsap.to('#hero', { opacity: 1, delay: 1.5 })
@@ -13,6 +17,14 @@ function Hero() {
     <section className='w-full nav-height bg-black relative'>
         <div className='h-5/6 w-full flex-center flex-col'>
             <p className='hero-title' id='hero'>iPhone 15 Pro</p>
+
+            <div className='md:w-10/12 w-9/12'>
+                <video className='pointer-events-none' 
+                  autoPlay muted playsInline={true} key={videoSrc}>
+                    <source src={videoSrc} type='video/mp4' />
+                </video>
+
+            </div>
 
         </div>
       
