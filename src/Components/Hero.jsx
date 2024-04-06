4d33a2a6fd2ec35ea18466864f0eb6543134888c
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-04-06 16:11:19 -0500

    basic CSS and text added into Hero

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>
+    <section className='w-full nav-height bg-black relative'>
+        <div className='h-5/6 w-full flex-center flex-col'>
+            <p className='hero-title'>iPhone 15 Pro</p>
+
+        </div>
       
-    </div>
+    </section>
   )
 }
 
