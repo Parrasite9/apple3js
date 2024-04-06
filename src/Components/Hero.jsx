eb1f1ba9427832e9fa2af01e7e42edc642bddacd
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-04-06 16:31:01 -0500

    First gsap animation created.

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 
 function Hero() {
+
+    useGSAP(() => {
+        gsap.to('#hero', { opacity: 1, delay: 1.5 })
+    }, [])
+
+
   return (
     <section className='w-full nav-height bg-black relative'>
         <div className='h-5/6 w-full flex-center flex-col'>
-            <p className='hero-title'>iPhone 15 Pro</p>
+            <p className='hero-title' id='hero'>iPhone 15 Pro</p>
 
         </div>
       
