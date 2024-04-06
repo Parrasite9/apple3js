30afb149abf1a5beea3931d43732d6488a1d7a78
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-04-06 17:03:26 -0500

    useEffect and functions related to resizing has been added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,10 +3,27 @@ import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 import { heroVideo, smallHeroVideo } from '../Utils/Index'
 import { useState } from 'react'
+import { useEffect } from 'react'
 
 function Hero() {
 
     const [videoSrc, setVideoSrc] = useState(Window.innerWidth < 760 ? smallHeroVideo : heroVideo)
+
+    const handleVideoSrcSet = () => {
+        if(window.innerWidth < 750) {
+            setVideoSrc(smallHeroVideo)
+        } else {
+            setVideoSrc(heroVideo)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleVideoSrcSet)
+
+        return () => {
+            window.removeEventListener('resize', handleVideoSrcSet)
+        }
+    }, [])
 
     useGSAP(() => {
         gsap.to('#hero', { opacity: 1, delay: 1.5 })
