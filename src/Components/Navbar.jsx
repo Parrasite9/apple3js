f9748fc1f7a1521e7a27ded4a9fadc15810065f9
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-04-06 15:50:49 -0500

    Navbar has been created without CSS.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { appleImg, searchImg, bagImg } from '../Utils/Index';
 
 function Navbar() {
   return (
-    <div className=''>
-      
-    </div>
+    <header>
+        <nav>
+            <img src={appleImg} alt="Apple" width={14} height={18} />
+            
+            <div>
+                {['Phones', 'Macbooks', 'Tablets'].map((nav) => (
+                    <div key={nav}>
+                        {nav}
+                    </div>
+                ))}
+            </div>
+
+            <div>
+                <img src={searchImg} alt="search" width={18} height={18} />
+
+                <img src={bagImg} alt="bag" width={18} height={18}/>
+            </div>
+        </nav>
+    </header>
   )
 }
 
