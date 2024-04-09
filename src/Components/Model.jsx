5d805d52804fce528d986a7f330217683ae96ec8
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-04-09 16:01:14 -0500

    Installed three JS and updated file as needed

--- src/Components/Model.jsx
+++ src/Components/Model.jsx
@@ -1,9 +1,27 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import React from 'react'
+import { useRef } from 'react'
+import { useState } from 'react'
+import { yellowImg } from '../Utils/Index'
 import ModelView from './ModelView'
 
+import * as THREE from 'three';
+
 function Model() {
+
+    const [size, setSize] = useState('small')
+    const [model, setModel] = useState({
+        title: 'iPhone 15 pro in Natural Titanium',
+        color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
+        img: yellowImg, 
+    })
+
+    // CAMERA CONTROL FOR THE MODEL VIEW 
+    const cameraControlSmall = useRef()
+    const cameraControlLarge = useRef()
+
+    const small = useRef(new THREE.Group())
 
     useGSAP(() => {
         gsap.to('#heading', {y: 0, opacity: 1})
