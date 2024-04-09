f8cbfc2550a0802c7117ee8c445647bfdae1b625
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-04-09 16:27:15 -0500

    Model now has all of the colors displayed as options and has CSS styling.

--- src/Components/Model.jsx
+++ src/Components/Model.jsx
@@ -7,6 +7,9 @@ import { yellowImg } from '../Utils/Index'
 import ModelView from './ModelView'
 
 import * as THREE from 'three';
+import { View } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { models } from '../Constants/Index'
 
 function Model() {
 
@@ -21,7 +24,14 @@ function Model() {
     const cameraControlSmall = useRef()
     const cameraControlLarge = useRef()
 
+
+    // MODEL 
     const small = useRef(new THREE.Group())
+    const large = useRef(new THREE.Group())
+
+    // ROTATION 
+    const [smallRotation, setSmallRotation] = useState(0)
+    const [largeRotation, setLargeRotation] = useState(0)
 
     useGSAP(() => {
         gsap.to('#heading', {y: 0, opacity: 1})
@@ -38,7 +48,53 @@ function Model() {
             <div className="flex flex-col items-center mt-5">
                 <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative"></div>
             </div>
-            <ModelView />
+            <ModelView 
+                index={1}
+                groupRef={small}
+                gsapType='view1'
+                controlRef={cameraControlSmall}
+                setRotationState={setSmallRotation}
+                item={model}
+                size={size}
+            />
+
+            <ModelView 
+                index={2}
+                groupRef={large}
+                gsapType='view2'
+                controlRef={cameraControlLarge}
+                setRotationState={setLargeRotation}
+                item={model}
+                size={size}
+            />
+
+            <Canvas className='w-full h-full'
+                    style={{
+                        position: 'fixed',
+                        top: 0,
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        overflow: 'hidden'
+                    }}
+                    eventSource={document.getElementById('root')}
+                    >
+                <View.Port />
+            </Canvas>
+        </div>
+
+        <div className="mx-auto w-full">
+            <p className='tx-sm font-light text-center mb-5'>{model.title}</p>
+            <div className="flex-center">
+                <ul className='color-container'>
+                    {models.map((item, i) => (
+                        <li key={i} className='w-6 h-6 rounded-full mx-2 cursor-pointer' style={{
+                            backgroundColor: item.color[0]}}
+                            onClick={() => setModel(item)} />
+                            
+                    ))}
+                </ul>
+            </div>
         </div>
       
     </section>
