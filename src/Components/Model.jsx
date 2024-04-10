738f9760ed3c8160157de0655d85923dba59d15a
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-04-09 20:48:46 -0500

    moved data around in Model in order to put said data in proper divs

--- src/Components/Model.jsx
+++ src/Components/Model.jsx
@@ -66,41 +66,43 @@ function Model() {
             </h1>
 
             <div className="flex flex-col items-center mt-5">
-                <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative"></div>
+                <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+                    <ModelView 
+                        index={1}
+                        groupRef={small}
+                        gsapType='view1'
+                        controlRef={cameraControlSmall}
+                        setRotationState={setSmallRotation}
+                        item={model}
+                        size={size}
+                    />
+
+                    <ModelView 
+                        index={2}
+                        groupRef={large}
+                        gsapType='view2'
+                        controlRef={cameraControlLarge}
+                        setRotationState={setLargeRotation}
+                        item={model}
+                        size={size}
+                    />
+
+                    <Canvas className='w-full h-full'
+                            style={{
+                                position: 'fixed',
+                                top: 0,
+                                bottom: 0,
+                                left: 0,
+                                right: 0,
+                                overflow: 'hidden'
+                            }}
+                            eventSource={document.getElementById('root')}
+                            >
+                        <View.Port />
+                    </Canvas>
+
+                </div>
             </div>
-            <ModelView 
-                index={1}
-                groupRef={small}
-                gsapType='view1'
-                controlRef={cameraControlSmall}
-                setRotationState={setSmallRotation}
-                item={model}
-                size={size}
-            />
-
-            <ModelView 
-                index={2}
-                groupRef={large}
-                gsapType='view2'
-                controlRef={cameraControlLarge}
-                setRotationState={setLargeRotation}
-                item={model}
-                size={size}
-            />
-
-            <Canvas className='w-full h-full'
-                    style={{
-                        position: 'fixed',
-                        top: 0,
-                        bottom: 0,
-                        left: 0,
-                        right: 0,
-                        overflow: 'hidden'
-                    }}
-                    eventSource={document.getElementById('root')}
-                    >
-                <View.Port />
-            </Canvas>
         </div>
 
         <div className="mx-auto w-full">
