8c2eaf1114cad02d9f0b662831393fe3365d9ebf
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-04-18 17:48:41 -0500

    completed the Features ssections and imported it into the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import Hero from "./Components/Hero"
 import Highlights from "./Components/Highlights"
 import Model from "./Components/Model"
 import Navbar from "./Components/Navbar"
+import * as Sentry from '@sentry/react'
+import Features from "./Components/Features"
 
 function App() {
-
   return (
     <main className="bg-black">
       <Navbar />
       <Hero/> 
       <Highlights />
       <Model />
-
+      <Features />
     </main>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App)
